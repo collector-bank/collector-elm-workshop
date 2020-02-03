fbb669ec5dad2e96aec2a90b4f5888b2cad01d15
Daniel Brännström <daniel.brannstrom@collectorbank.se>
2020-02-03 16:51:50 +0100

    .

---
+++ results/step3-newbutton/tests/MainTests.elm
@@ -0,0 +1,24 @@
+module MainTests exposing (..)
+
+import Expect exposing (Expectation)
+import Test exposing (..)
+import Test.Html.Query as Query
+import Test.Html.Selector exposing (text, tag)
+
+import Main exposing (..)
+
+updateTest =
+    describe "test of update function" 
+    [ test "increment message gives an incremented state" 
+        (\_ -> Expect.equal 1 (update Increment 0))
+    , test "decrement message gives an decremented state" 
+        (\_ -> Expect.equal 0 (update Decrement 1))
+    ]
+
+viewTest : Test
+viewTest = 
+    test "view should render counter" <| 
+        \_ -> view 5 
+            |> Query.fromHtml
+            |> Query.find [ tag "div" ]
+            |> Query.has [ text "5" ]
